4377c885b376637a1e0826108c5b1922b1c7236f
Krukas <maikel@martens.me>
2020-10-31 12:55:46 +0100

    [TASK] Update version

--- trionyx_accounts/__init__.py
+++ trionyx_accounts/__init__.py
@@ -5,6 +5,6 @@ trionyx_accounts
 :copyright: 2019 by Maikel Martens
 :license: GPLv3
 """
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 
 default_app_config = 'trionyx_accounts.apps.AccountsConfig'
